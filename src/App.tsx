0928d4b75712210457787a5b2470c853635b970e
Andrey Plahtiy <aplaxtiy@gmail.com>
2024-08-15 20:02:48 +0300

    add x 0

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,39 @@
 import React, {useState} from 'react';
 import './App.css';
 
-function App () {
-    const [actives, setActives] = useState(Array.from({length: 9}, (_) => "0"));
+function App() {
+    const [actives, setActives] = useState(Array.from({length: 9}, (_) => ""));
+    const [turn, setTurn] = useState(false)
 
+    const elements = Array.from({length: 9}, (_, index) => (
+        <div className="place" onClick={() => {
+            const newActives = [...actives];
+            newActives[index] = turn ? 'x' : "0";
+            setTurn(!turn)
+            setActives(newActives)
+        }}>
+            {actives[index]}
+        </div>
+    ));
 
-    const elements = Array.from({length: 9}, (_, index) =>
-        (
-            <div className="place" onClick={() => {
-                const newActives = [...actives];
-                newActives[index] = 'x';
-                setActives(newActives)
-            }}>
-                {actives[index]}
-            </div>
-        )
+    const winner = (
+        <div>
+            Player "X" is winner!
+        </div>
     );
 
     return (
-        <main>
-            <div className="game-container">
-                {elements}
-            </div>
-        </main>
+        <div className="container">
+            <main>
+                <header>
+                    <h1>Tic Tac Toe</h1>
+                </header>
+                <div className="game-container">
+                    {elements}
+                </div>
+                {winner}
+            </main>
+        </div>
     )
 }
 
